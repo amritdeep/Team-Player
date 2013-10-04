12f731bf68b668247668778ff5767967d7055a4d
Amritdeep Dhungana <Amritdeep@Amritdeeps-MacBook-Pro.local>
2013-10-03 23:02:44 -0500

    add list of player and favored player and complain about player

--- lib/team.rb
+++ lib/team.rb
@@ -1,3 +1,21 @@
 class Team
-	attr_accessor :name
+	attr_accessor :name, :players
+
+	def initialize(name, players = [])
+		raise Exception unless players.is_a? Array
+			
+		@name = name
+		@players = players
+
+		raise Exception if has_bad_name?
+	end
+
+	def has_bad_name?
+		list_to_words = %w{crappy bad lousy}
+		list_to_words - @name.downcase.split(" ") != list_to_words
+	end
+
+	def favored?
+		@players.include? "Amrit"
+	end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -2,13 +2,28 @@ require "./lib/team.rb"
 
 describe Team do 
 	it "has a name" do
-		Team.new.should respond_to :name
+		Team.new("Random name").should respond_to :name
 	end
 
 	it "has list of players" do
-		Team.new.should respond_to :players
+		Team.new("Random name").players.should be_kind_of Array 
 	end
 
-	it "is favored if it has a celebrity in it"
-	it "complains if there is a bad word in the name"
+	it "is favored if it has a celebrity in it" do
+		Team.new("Random name", ["Amrit", "Deep"]).should be_favored
+	end
+
+	it "complains if there is a bad word in the name" do
+		expect { Team.new("Crappy name") }.to raise_error
+
+	end
+
+	context "given a bad list of players" do
+		let(:bad_players) { {} }
+
+		it "fails to create given a bad player list" do
+			expect { Team.new("name", bad_players)}.to raise_error
+
+		end
+	end
 end
